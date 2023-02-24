753837a99ac4f82b0bd72ba02b7cff3bea0f1f62
Tito Alvarez <augustoalvarez@gmail.com>
2023-02-24 07:53:36 -0600

    Reg date and field naming

--- api/controllers/preregistrationController.js
+++ api/controllers/preregistrationController.js
@@ -6,96 +6,125 @@ const Concordium = require("../web3/concordium");
 const concordium = new Concordium();
 
 exports.add = async (req, res) => {
+  if (
+    ["id", "name", "product"].filter(
+      (field) => typeof req.body[field] === "undefined"
+    ).length
+  ) {
+    return res.status(406).json({ error: "Required fields are missing" }).end();
+  }
 
-    if (!req.body.id.match(/^@[a-z\d_]{3,20}$/i)) {
-        return res.status(406).json({error: 'The id is invalid'}).end();
+  if (
+    !["community", "starter", "team", "growth", "enterprise"].filter(
+      (product) => req.body.product === product
+    ).length
+  ) {
+    return res.status(406).json({ error: "Invalid product" }).end();
+  }
+
+  if (!req.body.id.match(/^@[a-z\d_]{3,20}$/i)) {
+    return res.status(406).json({ error: "Invalid id" }).end();
+  }
+
+  if (req.body.product.trim() !== "community" && !req.body.orderId) {
+    return res.status(406).json({ error: "Order id is required" }).end();
+  }
+
+  Object.entries(req.body).forEach(([key, val]) => {
+    const typeString = [
+      "id",
+      "name",
+      "email",
+      "organization",
+      "message",
+      "orderId",
+      "refShare2Earn",
+    ];
+
+    if (typeof val !== "string" && typeString.includes(key)) {
+      return res
+        .status(406)
+        .json({ error: key + " must be string" })
+        .end();
     }
+  });
 
-    if (req.body.product.trim() !== 'community' && !req.body.order_id)
-    {
-        return res.status(406).json({error: 'Order id is required'}).end();
-    }
+  const prereg = {
+    id: req.body.id,
+    name: req.body.name,
+    product: req.body.product,
+    dateReg: new Date(),
+  };
 
-    Object.entries(req.body).forEach(([key, val]) => {
+  ["organization", "message", "orderId", "refShare2Earn"].forEach((field) => {
+    if (typeof req.body[field] !== "undefined") prereg[field] = req.body[field];
+  });
 
-        let typeString = ['id', 'name', 'email', 'organization', 'message', 'order_id', 'refShare2Earn'];
+  try {
+    await Preregistration.create(prereg);
+  } catch (error) {
+    return res.status(406).json({ success: false }).end();
+  }
 
-        if ( typeof val !== "string" && typeString.includes(key))
-        {
-            return res.status(406).json({error: key + ' must be string'}).end();
-        }
-    });
-
-    try {
-        await Preregistration.create({
-            id: req.body.id,
-            name: req.body.name,
-            product: req.body.product,
-            organization: req.body.organization,
-            message: req.body.message,
-            order_id: req.body.order_id,
-            refShare2Earn: req.body.refShare2Earn,
-            })
-        } catch(error){
-         return res.status(406).json({success: false}).end();
-    }
-
-    res.status(201);
-    res.json({ success: true });
+  res.status(201);
+  res.json({ success: true });
 };
 
 exports.update = async (req, res) => {
-    const account   = req.params.account;
-    const signature = req.query.signature;
+  const account = req.params.account;
+  const signature = req.query.signature;
 
-    // Validate account in collection
-    Account.findOne({ address: account }, async (err, accountObj) => {
+  // Validate account in collection
+  Account.findOne({ address: account }, async (err, accountObj) => {
+    if (err) {
+      res.status(500).end();
+    }
 
-        if (err) {
-            res.status(500).end();
-        }
+    if (accountObj === null) {
+      res.status(404).end();
+    }
 
-        if (accountObj === null) {
-            res.status(404).end();
-        }
+    const nonce = accountObj.nonce;
 
-        const nonce = accountObj.nonce;
+    // Validate signature by concordium
+    if (
+      !(await concordium.validateAccount(
+        String(nonce),
+        JSON.parse(Buffer.from(signature, "base64").toString()),
+        account
+      ))
+    ) {
+      // Clear nonce in the account even signature verification failed
+      Account.updateOne({ address: account }, { nonce: null }, () => {});
+      res.status(403).end();
+    }
 
-        // Validate signature by concordium
-        if (!(await concordium.validateAccount(
-            String(nonce),
-            JSON.parse(Buffer.from(signature, "base64").toString()),
-            account ))
-        ) {
-            // Clear nonce in the account even signature verification failed
-            Account.updateOne({ address: account }, { nonce: null }, () => {});
-            res.status(403).end();
-        }
+    // Clear nonce in the account after signature verification
+    Account.updateOne({ address: account }, { nonce: null }, () => {});
+  });
 
-        // Clear nonce in the account after signature verification
-        Account.updateOne({ address: account }, { nonce: null }, () => {});
-    });
+  // Validate preregistration in collection
+  Preregistration.findOne({ id: req.params.id }, (err, preregistrationObj) => {
+    if (err) {
+      res.status(500).end();
+      return;
+    }
 
-    // Validate preregistration in collection
-    Preregistration.findOne({ id: req.params.id }, (err, preregistrationObj) => {
+    if (preregistrationObj === null) {
+      res.status(404).end();
+    }
 
-        if (err) {
-            res.status(500).end();
-            return;
-        }
+    // Validate Id already linked to another account
+    if (preregistrationObj.account && preregistrationObj.account !== account) {
+      res.status(406).end();
+    }
 
-        if (preregistrationObj === null) {
-            res.status(404).end();
-        }
-
-        // Validate Id already linked to another account
-        if (preregistrationObj.account && preregistrationObj.account !== account)
-        {
-            res.status(406).end();
-        }
-
-        Preregistration.updateOne({ id: req.params.id }, { account: account }, () => {
-            res.json({result: true}).status(201).end();
-        });
-    });
+    Preregistration.updateOne(
+      { id: req.params.id },
+      { account: account },
+      () => {
+        res.json({ result: true }).status(201).end();
+      }
+    );
+  });
 };
